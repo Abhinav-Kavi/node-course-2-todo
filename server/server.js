b680ffc4a41effe2d160bed912d922843d2ed497
Kavi <abkavi@deloitte.com>
2018-08-28 12:45:13 +0530

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} =  require('./models/todo');
@@ -20,6 +21,22 @@ app.get("/todos",(req,res)=>{
   Todo.find()
    .then(todos => res.send({todos}))
    .catch(e => res.status(400).send(e));
+});
+
+app.get("/todos/:id",(req,res)=>{
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id))
+   return res.status(404).send({'message':"Invalid todo Id"});
+ 
+  Todo.findById(id)
+   .then(todo => {
+     if(!todo)
+      return res.status(404).send({'message':"No todo exists with the given id"});
+      
+     res.send({todo});
+   })
+   .catch(err => res.status(400).send());
 });
 
 app.listen(3000, ()=>console.log("listening on port 3000 ..."));
